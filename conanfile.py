060b889088a3340d4d28656bb5a0eb66994096cd
Arthur Valadares Campideli <arthur.c.valadares@gmail.com>
2024-06-21 10:19:51 -0300

    feat: add the conan file and the respective project dependencies, that is, for example and until then, the locally compiled Catch2/3.6.0.

---
+++ conanfile.py
@@ -0,0 +1,48 @@
+import os
+from conan import ConanFile
+from conan.tools.meson import MesonToolchain, Meson
+from conan.tools.layout import basic_layout
+from conan.tools.files import copy
+
+
+class Conan(ConanFile):
+    name = "data-structure"
+    version = "0.1.0"
+    package_type = "library"
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "meson.build", "src/*", "include/*"
+
+    requires = "catch2/3.6.0"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.rm_safe("fPIC")
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        basic_layout(self)
+
+    def generate(self):
+        tc = MesonToolchain(self)
+        tc.generate()
+
+    def build(self):
+        meson = Meson(self)
+        meson.configure()
+        meson.build()
+
+    def package(self):
+        meson = Meson(self)
+        meson.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["data-structure"]
