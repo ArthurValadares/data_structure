28299a4ba8906e2f501b23c93a4aba4c2a9ea0d2
Arthur Valadares Campideli <arthur.c.valadares@gmail.com>
2024-08-23 14:05:16 -0300

    feat: Add header file for data structure includes
    Create `DataStructure.h` to centralize inclusion of common data structure headers like Common.h, Hash.h, Heap.h, List.h, Queue.h, Stack.h, and Tree.h. This facilitates easier management and ensures consistent inclusion across different parts of the project.

---
+++ include/DataStructure.h
@@ -0,0 +1,16 @@
+//
+// Created by arthur on 19/08/24.
+//
+
+#ifndef DATA_STRUCTURE_DATASTRUCTURE_H
+#define DATA_STRUCTURE_DATASTRUCTURE_H
+
+#include "common/Common.h"
+#include "hash/Hash.h"
+#include "heap/Heap.h"
+#include "list/List.h"
+#include "queue/Queue.h"
+#include "stack/Stack.h"
+#include "tree/Tree.h"
+
+#endif  // DATA_STRUCTURE_DATASTRUCTURE_H
