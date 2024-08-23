985946050f2c6d91953f8d28c47d652e6aa21f96
Arthur Valadares Campideli <arthur.c.valadares@gmail.com>
2024-08-23 14:05:01 -0300

    feat: Add Tree.h header file for tree data structures
    Introduce Tree.h as a central header file to include necessary tree-related classes, namely BinarySearchTree and Node. This provides a single point of inclusion, simplifying the management of tree data structures in the project.

---
+++ include/tree/Tree.h
@@ -0,0 +1,11 @@
+//
+// Created by arthur on 19/08/24.
+//
+
+#ifndef DATA_STRUCTURE_TREE_H
+#define DATA_STRUCTURE_TREE_H
+
+#include "binary_search_tree/BinarySearchTree.h"
+#include "common/node/Node.h"
+
+#endif  // DATA_STRUCTURE_TREE_H
