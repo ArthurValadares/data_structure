3f26d90a0fe284dfdbd950e89e119ed883a37691
Arthur Valadares Campideli <arthur.c.valadares@gmail.com>
2024-08-23 14:04:29 -0300

    feat: Add header for common tree node
    Introduce Node.h to encapsulate common includes for node types like AbstractDoublyLinkedBinaryTreeNode, AbstractSinglyLinkedBinaryTreeNode, DoublyLinkedBinaryTreeNode, and SinglyLinkedBinaryTreeNode. This centralizes and organizes header dependencies for tree nodes.

---
+++ include/tree/common/node/Node.h
@@ -0,0 +1,13 @@
+//
+// Created by arthur on 19/08/24.
+//
+
+#ifndef DATA_STRUCTURE_NODE_H
+#define DATA_STRUCTURE_NODE_H
+
+#include "AbstractDoublyLinkedBinaryTreeNode.h"
+#include "AbstractSinglyLinkedBinaryTreeNode.h"
+#include "DoublyLinkedBinaryTreeNode.h"
+#include "SinglyLinkedBinaryTreeNode.h"
+
+#endif  // DATA_STRUCTURE_NODE_H
