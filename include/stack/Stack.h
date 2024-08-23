92e0c73c7a76a544e572d8b56f3e26df7249c454
Arthur Valadares Campideli <arthur.c.valadares@gmail.com>
2024-08-23 14:04:11 -0300

    feat: Add Stack.h to include stack implementations
    Introduce the Stack.h header file to include essential stack-related classes. This header will organize and centralize the inclusion of various stack implementations for easier management and use.

---
+++ include/stack/Stack.h
@@ -0,0 +1,13 @@
+//
+// Created by arthur on 19/08/24.
+//
+
+#ifndef DATA_STRUCTURE_STACK_H
+#define DATA_STRUCTURE_STACK_H
+
+#include "AbstractArrayStack.h"
+#include "AbstractLinkedStack.h"
+#include "ArrayStack.h"
+#include "LinkedStack.h"
+
+#endif  // DATA_STRUCTURE_STACK_H
